c69427f55331501933feaeec67966361c8170354
rn-santos227 <rn_santos227@yahoo.com>
2023-09-13 23:20:54 +0800

    created audit table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('type');
-            $table->unsignedInteger('users_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
