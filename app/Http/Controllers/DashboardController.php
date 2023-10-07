b9bae5c22c074e616c000fa1f4ee8df0f26fa64c
rn-santos227 <rn_santos227@yahoo.com>
2023-10-07 22:25:55 +0800

    added dashboard count data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,18 +8,31 @@ use App\Models\Student;
 use App\Models\Teacher;
 use App\Models\School;
 
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function admin() {
+        $modules = Module::count();
+        $schools = School::count();
+        $students = Student::count();
+        $teachers = Teacher::count();
+        $subjects = Subject::count(); 
+
+        return response()->json([
+            'modules' => $modules,
+            'schools' => $schools,
+            'students' => $students,
+            'teachers' => $teachers,
+            'subjects' => $subjects
+        ]);
+    }
+
+    public function teacher(Request $request) {
 
     }
 
-    public function teacher() {
-
-    }
-
-    public function student() {
+    public function student(Request $request) {
 
     }
 }
