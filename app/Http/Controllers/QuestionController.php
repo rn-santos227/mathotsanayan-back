5b7ec2b191cbcb92bbaa46d82d7f15a6e2a7ac07
rn-santos227 <rn_santos227@yahoo.com>
2023-10-22 22:49:01 +0800

    update single question creation in question controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -94,6 +94,54 @@ class QuestionController extends Controller
             'subject_id' => $request->subject,
         ]);
 
+        $file_name = "question-".$question->id."module".$request->module.".png";
+
+        if (!Storage::exists('questions/'.$file_name)) {
+            Storage::disk('minio')->put('questions/'.$file_name, (string) $question->file);
+        }
+
+        foreach($request->options as $option) {
+            $new_option = Option::create([
+                'content' => $option['content'],
+                'module_id' => $request->module,
+                'subject_id' => $request->subject,
+                'question_id' => $question->id,
+            ]);
+            $filename_option = "option-".$new_option->id."-".$file_name;
+
+            if (!Storage::exists('questions/question'.$question->id."/options/".$filename_option)) {
+                Storage::disk('minio')->put('questions/question'.$question->id."/options/".$filename_option, (string) $option->file);
+            }
+        };
+
+        foreach($request->corrects as $correct) {
+            $new_correct =  Correct::create([
+                'content' => $correct['content'],
+                'module_id' => $request->module,
+                'subject_id' => $request->subject,
+                'question_id' => $question->id,
+            ]);
+            $file_name_correct = "correct-".$new_correct->id."-".$file_name;
+            
+            if (!Storage::exists('questions/question'.$question->id."/corrects/".$file_name_correct)) {
+                Storage::disk('minio')->put('questions/question'.$question->id."/corrects/".$file_name_correct, (string) $correct->file);
+            }
+        };
+
+        foreach($request->solutions  as $solution) {
+            $new_solution = Solution::create([
+                'solution' => $solution['content'],
+                'module_id' => $request->module,
+                'subject_id' => $request->subject,
+                'question_id' => $question->id,
+            ]);
+            $file_name_solution = "solution-".$new_solution->id."-".$file_name;
+
+            if (!Storage::exists('questions/question'.$question->id."/solutions/".$file_name_solution)) {
+                Storage::disk('minio')->put('questions/question'.$question->id."/solutions/".$file_name_solution, (string) $solution->file);
+            }
+        };
+
         return response([
             'question' => $question,
         ], 201);
