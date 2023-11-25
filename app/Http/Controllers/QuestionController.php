2309a45cd669b4fbbf2ef6c6e73e276584b7039a
rn-santos227 <rn_santos227@yahoo.com>
2023-11-25 22:26:23 +0800

    fixed uploading

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -68,37 +68,43 @@ class QuestionController extends Controller
             }
         }
 
-        $file_count = 0;
-        foreach($question_files as $question_file) {
-            $id = $file_questions_ids[$file_count];
-            $file_url = '/questions/question-'.$id.'/'.$question_file->getClientOriginalName();;
-
-            $question = Question::find($id);
-            $question->update(['file' => $file_url]);
-            Storage::disk('minio')->put($file_url, file_get_contents($question_file));
-            $file_count++;
+        if(isset($question_files)) {
+            $file_count = 0;
+            foreach($question_files as $question_file) {
+                $id = $file_questions_ids[$file_count];
+                $file_url = '/questions/question-'.$id.'/question-'.$id.'.'.$question_file->getClientOriginalExtension();;
+    
+                $question = Question::find($id);
+                $question->update(['file' => $file_url]);
+                Storage::disk('minio')->put($file_url, file_get_contents($question_file));
+                $file_count++;
+            }
         }
 
-        $file_count = 0;
-        foreach($option_files as $option_file) {
-            $id = $file_questions_ids[$file_count];
-            $file_url = '/options/option-'.$id.'/'.$option_file->getClientOriginalName();
-
-            $option = Option::find($id);
-            $option->update(['file' => $file_url]);
-            Storage::disk('minio')->put($file_url, file_get_contents($option_file));
-            $file_count++;
+        if(isset($option_files)) {
+            $file_count = 0;
+            foreach($option_files as $option_file) {
+                $id = $file_questions_ids[$file_count];
+                $file_url = '/options/option-'.$id.'/option-'.$id.'.'.$option_file->getClientOriginalExtension();
+    
+                $option = Option::find($id);
+                $option->update(['file' => $file_url]);
+                Storage::disk('minio')->put($file_url, file_get_contents($option_file));
+                $file_count++;
+            }
         }
 
-        $file_count = 0;
-        foreach($correct_files as $correct_file) {
-            $id = $file_questions_ids[$file_count];
-            $file_url = '/corrects/correct-'.$id.'/'.$correct_file->getClientOriginalName();
-
-            $correct = Correct::find($id);
-            $correct->update(['file' => $file_url]);
-            Storage::disk('minio')->put($file_url, file_get_contents($correct_file));
-            $file_count++;
+        if(isset($correct_files)) {
+            $file_count = 0;
+            foreach($correct_files as $correct_file) {
+                $id = $file_questions_ids[$file_count];
+                $file_url = '/corrects/correct-'.$id.'/correct-'.$id.'.'.$correct_file->getClientOriginalExtension();
+    
+                $correct = Correct::find($id);
+                $correct->update(['file' => $file_url]);
+                Storage::disk('minio')->put($file_url, file_get_contents($correct_file));
+                $file_count++;
+            }
         }
 
         $questions = Question::with('corrects', 'options')->where([
