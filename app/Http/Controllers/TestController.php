b634cd9ca3c19922177f833e84ad1b52bde3b12a
rn-santos227 <rn_santos227@yahoo.com>
2023-12-02 22:14:38 +0800

    add correct checker in test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -12,7 +14,19 @@ class TestController extends Controller
 
     public function submit(Request $request) {
         if($request->id) {
+            $question = Question::find($request->id);
+            $question->load('corrects');
+            $correct = 0;
 
+            foreach($question->corrects as $correct) {
+                if(strtolower($correct->content) == strtolower($request->content)) {
+                    $correct = 1;
+                }
+            }
+
+            return response([
+                'correct' => $correct,
+            ], 201);
         } else return response([
             'error' => 'Illegal Access',
         ], 500);
