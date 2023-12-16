4b4bf0aa5337603e2a76acbcbf1898bc7c82962c
rn-santos227 <rn_santos227@yahoo.com>
2023-12-16 21:40:59 +0800

    update teacher controller

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -54,47 +54,46 @@ class TeacherController extends Controller
     }
 
     public function update(TeacherRequest $request) {
-        if($request->id) {
-            $request->validated();
-            $teacher = Teacher::find($request->id);
-            
-            if(!empty($request->password)) {
-                $teacher->makeVisible('user_id');
-                $user = User::find($teacher->user_id);
-                $user->update([
-                    'email' => $request->email,
-                    'password' => $request->password,
-                ]);
-            }
-            
-            $teacher->update([
-                'first_name' => $request->first_name,
-                'middle_name' => $request->middle_name,
-                'last_name' => $request->last_name,
-                'suffix' => $request->suffix,
+        if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+
+        $request->validated();
+        $teacher = Teacher::find($request->id);
+        if(!$teacher) return response(['error' => 'Illegal Access',], 500); 
+        
+        if(!empty($request->password)) {
+            $teacher->makeVisible('user_id');
+            $user = User::find($teacher->user_id);
+            $user->update([
                 'email' => $request->email,
-                'contact_number' => $request->contact_number,
-                'school_id' => is_numeric($request->school) ? $request->school['id'] : $request->school_id,
+                'password' => $request->password,
             ]);
-            
-            $teacher->load('school');
-            return response([
-                'teacher' => $teacher,
-            ], 201);
-        } else return response([
-            'error' => 'Illegal Access',
-        ], 500);
+        }
+        
+        $teacher->update([
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'suffix' => $request->suffix,
+            'email' => $request->email,
+            'contact_number' => $request->contact_number,
+            'school_id' => is_numeric($request->school) ? $request->school['id'] : $request->school_id,
+        ]);
+        
+        $teacher->load('school');
+        return response([
+            'teacher' => $teacher,
+        ], 201);
     }
 
     public function delete(Request $request ){
-        if($request->id) {
-            $teacher = Teacher::find($request->id);
-            $teacher->delete();
-            return response([
-                'teacher' => $teacher,
-            ], 201);
-        } else return response([
-            'error' => 'Illegal Access',
-        ], 500); 
+        if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+
+        $teacher = Teacher::find($request->id);
+        if(!$teacher) return response(['error' => 'Illegal Access',], 500); 
+
+        $teacher->delete();
+        return response([
+            'teacher' => $teacher,
+        ], 201);
     }
 }
