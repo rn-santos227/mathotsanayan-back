48e9532ae4d5419b8b32e0e4d4e9642afd221ebe
rn-santos227 <rn_santos227@yahoo.com>
2023-12-16 21:34:07 +0800

    update section controller and option controller

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -36,35 +36,32 @@ class SectionController extends Controller
     }
 
     public function update(SectionRequest $request) {
-        if($request->id) {
-            $request->validated();
-            $section = Section::find($request->id);
-            $section->update([
-                'name' => $request->name,
-                'description' => $request->description,
-                'teacher_id' => $request->teacher,
-                'school_id' => $request->school,
-            ]);
+        if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
 
-            $section->load('teacher', 'school', 'students');
-            return response([
-                'section' => $section,
-            ], 201);
-        }  else return response([
-            'error' => 'Illegal Access',
-        ], 500);
+        $request->validated();
+        $section = Section::find($request->id);
+        if(!$section) return response(['error' => 'Illegal Access',], 500); 
+        $section->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'teacher_id' => $request->teacher,
+            'school_id' => $request->school,
+        ]);
+
+        $section->load('teacher', 'school', 'students');
+        return response([
+            'section' => $section,
+        ], 201);
     }
 
     public function delete(Request $request ){
-        if($request->id) {
-            $section = Section::find($request->id);
-            $section->delete();
-            return response([
-                'section' => $section,
-            ], 201);
-        } 
-        else return response([
-            'error' => 'Illegal Access',
-        ], 500); 
+        if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+
+        $section = Section::find($request->id);
+        if(!$section) return response(['error' => 'Illegal Access',], 500); 
+        $section->delete();
+        return response([
+            'section' => $section,
+        ], 201);
     }
 }
