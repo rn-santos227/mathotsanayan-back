5211fafeb727345a851c758b392d198ad8ebf1f7
rn-santos227 <rn_santos227@yahoo.com>
2023-12-27 21:41:04 +0800

    update admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\User;
 
 use App\Http\Requests\AdminRequest;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\StudentMail;
 
 class AdminController extends Controller
 {
@@ -28,13 +32,30 @@ class AdminController extends Controller
             'password' => $request->password,
         ]);
 
+        $admin = Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'contact_number' => $request->contact_number,
+            'user_id' => $user->id,
+        ]);
+
+        $username = $request->email;
+        $password = $request->password;
+
+        Mail::to($request->email)->send(new AdminMail($username, $password));
+
+        return response([
+            'admin' => $admin,
+        ], 201);
+
     }
 
     public function update(AdminRequest $request) {
         $request->validated();
+        $admin = Admin::find($request->id);
     }
 
     public function delete(AdminRequest $request ){
-
+        $admin = Admin::find($request->id);
     }
 }
