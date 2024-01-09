1cda70b5e12bc016d96984ecf100f3a9dcec1fc3
rn-santos227 <rn_santos227@yahoo.com>
2024-01-09 21:04:22 +0800

    update auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,108 +11,111 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function admin(Request $request) {
-        $fields = User::validate($request);
-        $user = User::where('email', $fields['email'])->first();
+  public function admin(Request $request) {
+    $fields = User::validate($request);
+    $user = User::where('email', $fields['email'])->first();
 
-        if(!$user || !$user->validatePassword($fields['password']) || $user->type > 1) {
-            return response([
-                'message' => 'Bad Credentials'
-            ], 401);
-        }
-
-        $admin = Admin::where([
-            "user_id" => $user->id,
-        ])->first();
-        return $user->getToken($user, $admin, "admin");
+    if(!$user || !$user->validatePassword($fields['password']) || $user->type > 1) {
+      return response([
+        'message' => 'Bad Credentials'
+      ], 401);
     }
 
-    public function teacher(Request $request) {
-        $fields = User::validate($request);
-        $user = User::where('email', $fields['email'])->first();
+    $admin = Admin::where([
+      "user_id" => $user->id,
+    ])->first();
+    return $user->getToken($user, $admin, "admin");
+  }
 
-        if(!$user || !$user->validatePassword($fields['password']) || $user->type > 2) {
-            return response([
-                'message' => 'Bad Credentials'
-            ], 401);
-        }
+  public function teacher(Request $request) {
+    $fields = User::validate($request);
+    $user = User::where('email', $fields['email'])->first();
 
-        $teacher = Teacher::where([
-            "user_id" => $user->id,
-        ])->first();
-
-        if($teacher) {
-            $teacher->load('school');
-        }
-
-        return $user->getToken($user, $teacher, "teacher");
+    if(!$user || !$user->validatePassword($fields['password']) || $user->type > 2) {
+      return response([
+        'message' => 'Bad Credentials'
+      ], 401);
     }
 
-    public function student(Request $request) {
-        $fields = User::validate($request);
-        $user = User::where('email', $fields['email'])->first();
+    $teacher = Teacher::where([
+      "user_id" => $user->id,
+    ])->first();
 
-        if(!$user || !$user->validatePassword($fields['password'])) {
-            return response([
-                'message' => 'Bad Credentials'
-            ], 401);
-        }
+    if($teacher) {
+      $teacher->load('school');
+    }
+
+    return $user->getToken($user, $teacher, "teacher");
+  }
+
+  public function student(Request $request) {
+    $fields = User::validate($request);
+    $user = User::where('email', $fields['email'])->first();
+
+    if(!$user || !$user->validatePassword($fields['password'])) {
+      return response([
+        'message' => 'Bad Credentials'
+      ], 401);
+    }
+    
+    $student = Student::where([
+      "user_id" => $user->id,
+    ])->first();
+
+    if($student) {
+      $student->load('course','section','school','section.teacher');
+    }
+
+    return $user->getToken($user, $student, "student");
+  }
+
+  public function auth() {
+    return [
+      'auth' => auth('sanctum')->check(),
+    ];
+  }
+
+  public function user(Request $request) {
+    if($request->type) {
+      $user = auth('sanctum')->user();
+      switch ($request->type) {
+        case 1:
+          $admin = Admin::where([
+              "user_id" => $user->id,
+          ])->first();
+          return [
+              'admin' => $admin
+          ];
         
-        $student = Student::where([
-            "user_id" => $user->id,
-        ])->first();
+        case 2:
+          $teacher = Teacher::where([
+              "user_id" => $user->id,
+          ])->first();
+  
+          $teacher->load('school');
+          return [
+              'teacher' => $teacher
+          ];
+    
+        default:
+          $student = Student::where([
+              "user_id" => $user->id,
+          ])->first();
+  
+          $student->load('course','section','school','section.teacher');
+          return [
+              'student' => $student
+          ];
+      }
+    } else return response([
+      'error' => 'Illegal Access',
+    ], 500); 
+  }
 
-        if($student) {
-            $student->load('course','section','school','section.teacher');
-        }
-
-        return $user->getToken($user, $student, "student");
-    }
-
-    public function auth() {
-        return [
-            'auth' => auth('sanctum')->check(),
-        ];
-    }
-
-    public function user(Request $request) {
-        if($request->type) {
-            $user = auth('sanctum')->user();
-            if($request->type == 1) {
-                $admin = Admin::where([
-                    "user_id" => $user->id,
-                ])->first();
-                return [
-                    'admin' => $admin
-                ];
-            } else if ($request->tye == 2) {
-                $teacher = Teacher::where([
-                    "user_id" => $user->id,
-                ])->first();
-
-                $teacher->load('school');
-                return [
-                    'teacher' => $teacher
-                ];
-            } else {
-                $student = Student::where([
-                    "user_id" => $user->id,
-                ])->first();
-
-                $student->load('course','section','school','section.teacher');
-                return [
-                    'student' => $student
-                ];
-            }
-        } else return response([
-            'error' => 'Illegal Access',
-        ], 500); 
-    }
-
-    public function logout(Request $request) {
-        $request->user()->tokens()->delete();
-        return [
-            'message' => 'Logged Out'
-        ];
-    }
+  public function logout(Request $request) {
+    $request->user()->tokens()->delete();
+    return [
+      'message' => 'Logged Out'
+    ];
+  }
 }
