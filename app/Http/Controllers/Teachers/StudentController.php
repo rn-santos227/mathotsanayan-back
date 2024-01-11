cecb2226014635016742065520dbbe90a85bc00d
rn-santos227 <rn_santos227@yahoo.com>
2024-01-11 23:00:44 +0800

    update teacher students controller index

--- app/Http/Controllers/Teachers/StudentController.php
+++ app/Http/Controllers/Teachers/StudentController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Teachers;
 
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\User;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,5 +13,27 @@ class StudentController extends Controller
 {
   public function __construct() {
     $this->middleware('auth:sanctum');
+  }
+
+  public function index() {
+    $user = auth('sanctum')->user();
+    $teacher = Teacher::where([
+      "user_id" => $user->id,
+    ])->first();
+
+    $students = Student::with('section','school','course')
+    ->where(function($query) use($teacher) {
+      $query->whereHas('section', function ($query) use ($teacher) {
+        $query->where([
+          'teacher_id' => $teacher->id
+        ]);
+      });
+    })
+    ->orderBy('created_at', 'desc')
+    ->paginate(10);
+
+    return response()->json([
+      'students' => $students
+    ]);
   }
 }
