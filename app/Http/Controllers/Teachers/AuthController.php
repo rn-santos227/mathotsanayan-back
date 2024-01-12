07147a79af8c7d7232e38e2674d25050f832c20b
rn-santos227 <rn_santos227@yahoo.com>
2024-01-13 00:36:20 +0800

    updated teacher auth controller

--- app/Http/Controllers/Teachers/AuthController.php
+++ app/Http/Controllers/Teachers/AuthController.php
@@ -1,11 +1,51 @@
 <?php
 
 namespace App\Http\Controllers\Teachers;
-
 use App\Http\Controllers\Controller;
+
+use App\Models\Teacher;
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+  public function index(Request $request) {
+    $fields = User::validate($request);
+    $user = User::where('email', $fields['email'])->first();
+
+    if(!$user || !$user->validatePassword($fields['password']) || $user->type != 1) {
+      return response([
+        'message' => 'Bad Credentials'
+      ], 401);
+    }
+
+    $teacher = Teacher::where([
+      "user_id" => $user->id,
+    ])->first();
+    return $user->getToken($user, $teacher, "teacher");
+  }
+
+  public function user(Request $request) {
+    $user = auth('sanctum')->user();
+    if ($user->type != 1) {
+      return response(['error' => 'Illegal Access'], 500);
+    }
+
+    $teacher = Teacher::where('user_id', $user->id)->first();
+    return ['teacher' => $teacher];
+  }
+
+  public function auth() {
+    return [
+      'auth' => auth('sanctum')->check(),
+    ];
+  }
+
+  public function logout(Request $request) {
+    $request->user()->tokens()->delete();
+    return [
+      'message' => 'Logged Out'
+    ];
+  }
 }
