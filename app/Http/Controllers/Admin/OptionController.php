e51a2b1a4b931433ad743b4ca3793c8bd48b6b96
rn-santos227 <rn_santos227@yahoo.com>
2024-01-12 23:33:46 +0800

    added opetion controller in admin folder

--- app/Http/Controllers/Admin/OptionController.php
+++ app/Http/Controllers/Admin/OptionController.php
@@ -1,11 +1,100 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Storage;
+
+use App\Models\Question;
+use App\Models\Option;
+
 use Illuminate\Http\Request;
 
 class OptionController extends Controller
 {
-    //
+  public function __construct() {
+    $this->middleware('auth:sanctum');
+  }
+
+  public function create(Request $request) {
+    if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+
+    $question = Question::find($request->id);
+    if(!$question) return response(['error' => 'Illegal Access',], 500); 
+    $payload_option = json_decode($request->option, true);
+
+    $option = Option::create([
+      'content' => $payload_option['content'],
+      'module_id' => $question->module_id,
+      'subject_id' => $question->subject_id,
+      'question_id' => $question->id,
+    ]);
+
+    $file = $request->file('option_file');
+    if(isset($file)) {
+      $file_url = '/options/option-'.$option->id.'/option-'.$option->id.'.'.$file->getClientOriginalExtension();
+
+      if (Storage::disk('minio')->exists($file_url)) {
+        Storage::disk('minio')->delete($file_url);
+      }
+      
+      $option->update(['file' => $file_url]);
+      Storage::disk('minio')->put($file_url, file_get_contents($file));
+    }
+
+    $questions = Question::with('corrects', 'options')->where([
+      'module_id' => $question->module_id,
+    ])->get();
+
+    return response([
+      'questions' => $questions,
+    ], 201);
+  }
+
+  public function update(Request $request) {
+    if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+    $payload_option = json_decode($request->option, true);
+    $option = Option::find($request->id);
+
+    if(!$option) return response(['error' => 'Illegal Access',], 500); 
+    $option->update([
+      'content' => $payload_option['content'],
+    ]);
+
+    $file = $request->file('option_file');
+    if(isset($file)) {
+      $file_url = '/options/option-'.$option->id.'/option-'.$option->id.'.'.$file->getClientOriginalExtension();
+
+      if (Storage::disk('minio')->exists($file_url)) {
+        Storage::disk('minio')->delete($file_url);
+      }
+      
+      $option->update(['file' => $file_url]);
+      Storage::disk('minio')->put($file_url, file_get_contents($file));
+    }
+
+    $question = Question::find($option->question_id);
+    $questions = Question::with('corrects', 'options')->where([
+      'module_id' => $question->module_id,
+    ])->get();
+
+    return response([
+      'questions' => $questions,
+    ], 201);
+  }
+
+  public function delete(Request $request ) { 
+    if(!$request->id) return response(['error' => 'Illegal Access',], 500); 
+    $option = Option::find($request->id);
+    if(!$option) return response(['error' => 'Illegal Access',], 500); 
+
+    $question = Question::find($option->question_id);
+    $questions = Question::with('corrects', 'options')->where([
+      'module_id' => $question->module_id,
+    ])->get();
+    $option->delete();
+    return response([
+      'questions' => $questions,
+    ], 201);
+  }
 }
