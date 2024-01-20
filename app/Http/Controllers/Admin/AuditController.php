7e4f54a65d86052aa30a86a641ec9a357f91036c
rn-santos227 <rn_santos227@yahoo.com>
2024-01-20 17:32:39 +0800

    added search in admin audit

--- app/Http/Controllers/Admin/AuditController.php
+++ app/Http/Controllers/Admin/AuditController.php
@@ -22,4 +22,36 @@ class AuditController extends Controller
       'audit' => $audit
     ], 200);
   }
+
+  public function search(Request $request) {
+    if(!$request->query('category')) return response(['error' => 'Illegal Access'], 500);
+    $audit = Audit::with('user')
+    ->where(function($query) use($request) {
+      $category = $request->query('category');
+      $search = $request->query('search');
+      switch ($category) {
+        case 'activity':
+          $query->where('activity', 'like', '%' . $search . '%');
+          break;
+
+        case 'content':
+          $query->where('content', 'like', '%' . $search . '%');
+          break;
+
+        case 'created_at':
+          $query->where('created_at', 'like', '%' . $search . '%');
+          break;
+
+        case 'table':
+          $query->where('table', 'like', '%' . $search . '%');
+          break;
+      }
+    })
+    ->orderBy('created_at', 'desc')
+    ->get();
+
+    return response()->json([
+      'audit' => $audit,
+    ]);
+  }
 }
