b9812a73849c74edf3cf5470b8971d09007f5698
rn-santos227 <rn_santos227@yahoo.com>
2024-01-23 21:34:42 +0800

    update student controller in teacher

--- app/Http/Controllers/Teachers/StudentController.php
+++ app/Http/Controllers/Teachers/StudentController.php
@@ -105,7 +105,13 @@ class StudentController extends Controller
 
       switch ($category) {
         case 'full_name':
-          $query->whereRaw("CONCAT(last_name, ', ', first_name, ' ', COALESCE(suffix, ''), ' ', UPPER(SUBSTRING(middle_name, 1, 1))) LIKE ?", ['%' . $search . '%']);
+          $search = '%' . $search . '%';   
+          $query->where(function ($query) use ($search) {
+            $query->where('last_name', 'LIKE', $search)
+            ->orWhere('first_name', 'LIKE', $search)
+            ->orWhere('suffix', 'LIKE', $search)
+            ->orWhereRaw("UPPER(SUBSTRING(middle_name, 1, 1)) LIKE ?", [strtoupper(substr($search, 1, 1))]);
+          });
           break;
 
         case 'email':
