56c2e0d46c27fe2e8f9a2535a0108485e6a23a0a
rn-santos227 <rn_santos227@yahoo.com>
2024-01-26 22:49:26 +0800

    added ratio function in admin dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -43,6 +43,30 @@ class DashboardController extends Controller
   }
 
   public function ratio() {
-    
+    $graph = [
+      'passed' => 0,
+      'failed' => 0,
+    ];
+
+    $results = Result::where([
+      'completed' => 1,
+      'invalidate' => 0,
+    ])->whereHas('student', function ($query) {
+      $query->whereNull('students.deleted_at');
+    })->get();
+    $results->makeVisible(['total_score', 'grade', 'module']);
+
+    foreach ($results as $result) {
+      $module = $result->module;
+      if($result->grade >= $module->passing) {
+        $graph['passed'] =  $graph['passed'] + 1;
+      } else {
+        $graph['failed'] =  $graph['failed'] + 1;
+      }
+    }
+
+    return response()->json([
+      'graph' => $graph
+    ]);
   }
 }
