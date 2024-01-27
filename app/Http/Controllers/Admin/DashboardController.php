c7f5a57b733c1b58655be00e5bce39aa27e92443
rn-santos227 <rn_santos227@yahoo.com>
2024-01-27 19:01:58 +0800

    added modules in dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -69,4 +69,37 @@ class DashboardController extends Controller
       'graph' => $graph
     ]);
   }
+
+  public function modules() {
+    $modules = Module::get();
+    $result_modules = [];
+
+    foreach($modules as $module) {
+      $result_module = [
+        'modules' => $module,
+        'passed' => 0,
+        'failed' => 0,
+      ];
+
+      $results = Result::where([
+        'completed' => 1,
+        'invalidate' => 0,
+        'module_id' => $module->id,
+      ])->get();
+
+      foreach ($results as $result) {
+        $module = $result->module;
+        if($result->grade >= $module->passing) {
+          $result_module['passed'] =  $result_module['passed'] + 1;
+        } else {
+          $result_module['failed'] =  $result_module['failed'] + 1;
+        }
+      }
+      array_push($result_modules, $result_module);
+    }
+
+    return response()->json([
+      'result_modules' => $result_modules
+    ]);
+  }
 }
