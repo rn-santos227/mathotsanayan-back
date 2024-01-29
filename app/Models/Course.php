633319699a4affe61205b47fbb7e28f75140efbe
rn-santos227 <rn_santos227@yahoo.com>
2024-01-29 21:56:33 +0800

    update course model and course controller

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Student;
+
 use App\Traits\Auditable;
 class Course extends Model
 {
@@ -15,4 +17,8 @@ class Course extends Model
         'abbreviation',
         'description',
     ];
+
+    public function students() {
+        return $this->hasMany(Student::class);
+    }
 }
