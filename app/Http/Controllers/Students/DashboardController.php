21b807392153e9cc979636e2d51c4b91c456c82f
rn-santos227 <rn_santos227@yahoo.com>
2024-02-06 23:13:40 +0800

    update dashboard controller in student

--- app/Http/Controllers/Students/DashboardController.php
+++ app/Http/Controllers/Students/DashboardController.php
@@ -49,29 +49,32 @@ class DashboardController extends Controller
     $user = auth('sanctum')->user();
     $student = Student::where('user_id', $user->id)->first();
     
-    $results = Result::with('module', 'progress')
-    ->where('student_id', $student->id)
-    ->where('completed', 1)
-    ->where('invalidate', 0)
-    ->whereHas('module', function ($query) {
-      $query->where('active', 1);
-    })
-    ->get()
-    ->makeVisible(['timer', 'completed', 'total_score']);
-    
-    $passed = $failed = 0;
-    
+    $graph = [
+      'passed' => 0,
+      'failed' => 0,
+    ];
+
+    $results = Result::where([
+      'completed' => 1,
+      'invalidate' => 0,
+      'student_id' => $student->id,
+    ])->get();
+
+    $results->makeVisible(['total_score', 'grade', 'module']);
+
     foreach ($results as $result) {
-      $grade = ($result->total_score / $result->items) * 100;
-      ($grade >= $result->module->passing) ? $passed++ : $failed++;
+      $module = $result->module;
+      if($result->grade >= $module->passing) {
+        $graph['passed'] =  $graph['passed'] + 1;
+      } else {
+        $graph['failed'] =  $graph['failed'] + 1;
+      }
     }
+
     
     return response()->json([
-      'dashboard' => [
-        'passed' => $passed,
-        'failed' => $failed,
-      ],
-    ], 200);
+      'graph' => $graph
+    ]);
   }
 
   public function modules() {
