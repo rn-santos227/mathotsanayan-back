8be7ff70bda33572bf30fda601c423146a11f954
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 20:44:40 +0800

    added search function in modules controller from teacher

--- app/Http/Controllers/Teachers/ModuleController.php
+++ app/Http/Controllers/Teachers/ModuleController.php
@@ -24,4 +24,34 @@ class ModuleController extends Controller
       'modules' => $modules
     ]);
   }
+  
+  public function search(Request $request) {
+    if(!$request->query('category')) return response(['error' => 'Illegal Access'], 500);
+    $modules = Module::with('subject', 'questions', 'questions.corrects', 'questions.options')
+    ->where(function($query) use($request) {
+      $category = $request->query('category');
+      $search = $request->query('search');
+      switch ($category) {
+        case 'name':
+          $query->where('name', 'like', '%' . $search . '%');
+          break;
+  
+        case 'subject.name':
+          $query->whereHas('subject', function ($query) use ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
+          });
+          break;
+    
+        case 'description':
+          $query->where('name', 'like', '%' . $search . '%');
+          break;
+      }
+    })
+    ->orderBy('created_at', 'desc')
+    ->get();
+
+    return response()->json([
+      'modules' => $modules,
+    ]);
+  }
 }
