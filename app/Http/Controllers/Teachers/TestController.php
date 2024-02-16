cb98df0a1d561b0356202d92cf90373bdb71e902
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 21:38:03 +0800

    added check solution in test controller in teacher

--- app/Http/Controllers/Teachers/TestController.php
+++ app/Http/Controllers/Teachers/TestController.php
@@ -38,6 +38,12 @@ class TestController extends Controller
       }
     }
 
+    if(!$check) {
+      $solution = Correct::where([
+        'question_id' => $question->id
+      ])->inRandomOrder()->first();
+    }
+
     return response([
       'solution' => $solution,
       'correct' => $check,
