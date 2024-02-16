5814726c66b69d8cbb40a86ca4d4e8c9f91cbfa3
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 20:20:50 +0800

    added get modules in modules controller from teacher

--- app/Http/Controllers/Teachers/ModuleController.php
+++ app/Http/Controllers/Teachers/ModuleController.php
@@ -5,9 +5,23 @@ namespace App\Http\Controllers\Teachers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Module;
+
 class ModuleController extends Controller
 {
   public function __construct() {
     $this->middleware('auth:sanctum');
+  }
+
+  public function index() {
+    $modules = Module::with('subject', 'questions', 'questions.corrects', 'questions.options')
+    ->where([
+      "active" => 1,
+    ])
+    ->orderBy('created_at', 'desc')->paginate(10);
+    
+    return response()->json([
+      'modules' => $modules
+    ]);
   }
 }
