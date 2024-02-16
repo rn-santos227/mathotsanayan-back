8d58126a4e72289ab7ec2463ec60cc2ac7b89e52
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 20:52:55 +0800

    added index function in qustion controller on teacher

--- app/Http/Controllers/Teachers/QuestionController.php
+++ app/Http/Controllers/Teachers/QuestionController.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers\Teachers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Module;
+use App\Models\Question;
+
 class QuestionController extends Controller
 {
   public function __construct() {
     $this->middleware('auth:sanctum');
+  }
+
+  public function index(Request $request) {
+    if(!$request->id) return response([
+      'error' => 'Illegal Access',
+    ], 500); 
+    $module = Module::find($request->id);
+    if (!$module) return response(['error' => 'Illegal Access'], 500);
+
+    $questions = Question::with('corrects','options')->where([
+      'module_id' => $module->id
+    ])->get();
+
+    return response([
+      'questions' => $questions,
+    ], 201);
   }
 }
