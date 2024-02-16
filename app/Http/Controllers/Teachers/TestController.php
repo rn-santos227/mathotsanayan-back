0c71e9b22f0aebf8ef09800e1d6701bc4fb55e8e
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 20:58:16 +0800

    added test request

--- app/Http/Controllers/Teachers/TestController.php
+++ app/Http/Controllers/Teachers/TestController.php
@@ -5,10 +5,41 @@ namespace App\Http\Controllers\Teachers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Correct;
+use App\Models\Question;
+
 class TestController extends Controller
 {
   public function __construct() {
     $this->middleware('auth:sanctum');
   }
 
+  public function submit(Request $request) {
+    if (!$request->id) {
+      return response(['error' => 'Illegal Access'], 500);
+    }
+
+    $question = Question::find($request->id);
+    $question->load('corrects');
+    $question->testQuestion($question);
+    
+    $check = false;
+    $solution = null;
+
+    foreach ($question->corrects as $correct) {
+      $trim_answer = trim($request->content);
+      $trim_correct = trim($correct->content);
+
+      if (strtolower($trim_answer) == strtolower($trim_correct)) {
+        $check = true;
+        $solution = Correct::find($correct->id);
+        break;
+      }
+    }
+
+    return response([
+      'solution' => $solution,
+      'correct' => $check,
+    ], 201);
+  }
 }
---
+++ app/Http/Requests/TestRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Question;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class TestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (!auth()->check()) {
+            return false;
+        }
+
+        if (!$this->id || !Question::find($this->id)) {
+            throw new HttpResponseException(response([
+                'error' => 'Illegal Access',
+            ], 500));
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
