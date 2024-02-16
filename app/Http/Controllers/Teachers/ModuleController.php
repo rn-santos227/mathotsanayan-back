15adbac7f3c7ac8352704634a4248106806d77c2
rn-santos227 <rn_santos227@yahoo.com>
2024-02-16 20:00:59 +0800

    added middleware for module and question controller

--- app/Http/Controllers/Teachers/ModuleController.php
+++ app/Http/Controllers/Teachers/ModuleController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    //
+  public function __construct() {
+    $this->middleware('auth:sanctum');
+  }
 }
--- app/Http/Controllers/Teachers/QuestionController.php
+++ app/Http/Controllers/Teachers/QuestionController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+  public function __construct() {
+    $this->middleware('auth:sanctum');
+  }
 }
