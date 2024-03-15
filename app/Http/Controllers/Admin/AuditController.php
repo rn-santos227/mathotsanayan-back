f4ae053c4b7fe89706b48e08218ff6aaa11f2a57
rn-santos227 <rn_santos227@yahoo.com>
2024-03-15 21:39:52 +0800

    update audit controller in admin

--- app/Http/Controllers/Admin/AuditController.php
+++ app/Http/Controllers/Admin/AuditController.php
@@ -33,42 +33,39 @@ class AuditController extends Controller
 
   public function search(Request $request) {
     if(!$request->query('category')) return response(['error' => 'Illegal Access'], 500);
-    $audit = Audit::with('user')
-    ->where(function($query) use($request) {
-      $category = $request->query('category');
-      $search = $request->query('search');
-      switch ($category) {
-        case 'user.full_name';
-          $query->whereHas('user', function ($userQuery) use ($search) {
-            $userQuery->whereHas('owner', function ($ownerQuery) use ($search) {
-                $ownerQuery->whereRaw("CONCAT(last_name, ', ', first_name, ' ', COALESCE(suffix, ''), ' ', UPPER(SUBSTRING(middle_name, 1, 1))) LIKE ?", ['%' . $search . '%']);
-            });
-          });
-          break;
+    if($request->query('category') === 'user.full_name') {
+      
+    } else {
+      $audit = Audit::with('user')
+      ->where(function($query) use($request) {
+        $category = $request->query('category');
+        $search = $request->query('search');
+        switch ($category) {
+          case 'activity':
+            $query->where('activity', 'like', '%' . $search . '%');
+            break;
+  
+          case 'content':
+            $query->where('content', 'like', '%' . $search . '%');
+            break;
+  
+          case 'date':
+            if (preg_match('/^\d{4}(-\d{2})?$/', $search)) {
+                $query->where('created_at', 'like', $search . '%');
+            } else {
+                $query->whereDate('created_at', now()->toDateString());
+            }
+            break;
+  
+          case 'table':
+            $query->where('table', 'like', '%' . $search . '%');
+            break;
+        }
+      })
+      ->orderBy('created_at', 'desc')
+      ->get();
+    }
 
-        case 'activity':
-          $query->where('activity', 'like', '%' . $search . '%');
-          break;
-
-        case 'content':
-          $query->where('content', 'like', '%' . $search . '%');
-          break;
-
-        case 'date':
-          if (preg_match('/^\d{4}(-\d{2})?$/', $search)) {
-              $query->where('created_at', 'like', $search . '%');
-          } else {
-              $query->whereDate('created_at', now()->toDateString());
-          }
-          break;
-
-        case 'table':
-          $query->where('table', 'like', '%' . $search . '%');
-          break;
-      }
-    })
-    ->orderBy('created_at', 'desc')
-    ->get();
 
     foreach ($audit as $auditRecord) {
       $content = json_decode($auditRecord->content, true);
